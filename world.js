d30d7078d84987f4c2aa3ccbd93e6a2894e17242
Ahmalapala <ahmalchambers27@gmail.com>
2022-11-14 20:35:32 -0500

    changes were made

---
+++ world.js
@@ -0,0 +1,39 @@
+window.onload=function(){
+    let message = document.getElementById('result');
+
+    let countryBtn = document.getElementById('lookup');
+    countryBtn.addEventListener('click',function(action){
+        action.preventDefault();
+        let countryReq = document.getElementById("country").value;
+        fetch("world.php" + "?country=" + countryReq)
+        .then(response =>{
+            if (response.ok){
+                return response.text()
+            }else{
+                return Promise.reject("There was an issue with the request.")
+            }
+        })
+        .then(data => {
+            message.innerHTML = data;
+        })
+        .catch(error => console.log("Error: " + error));
+    });
+
+    let cityBtn = document.getElementById('lookupCity');
+    cityBtn.addEventListener('click',function(action){
+        action.preventDefault();
+        let cityReq = document.getElementById("country").value;
+        fetch("world.php" + "?country=" + cityReq + "&context=cities")
+        .then(response =>{
+            if (response.ok){
+                return response.text()
+            } else{
+                return Promise.reject("There was issue with the request.")
+            }
+        })
+        .then(data => {
+            message.innerHTML = data;
+        })
+        .catch(error => console.log("Error: " + error));
+    });
+}
